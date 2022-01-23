5caa9d6d9767e4a1b7535067895d5e03bd479066
Dalikk <dmitriev.alik1999@gmail.com>
2022-01-23 22:38:31 +0900

    добавлены static файлы и указан путь в settings.py

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR.joinpath('static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
